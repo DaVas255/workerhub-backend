ad87c035e316011c4c1dd652e258c825010d17e6
Danil <DaVas255@gmail.com>
2024-04-06 20:31:55 +0700

    add login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'prisma/prisma.service';
+
+export const jwtSecret = 'zjP9h6ZI5LoSKCRj';
+
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtSecret,
+      signOptions: { expiresIn: '5m' }, // e.g. 30s, 7d, 24h
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService],
 })
 export class AuthModule {}
