26ae1cf39c4b6e980b7ad36de5a964da034ffed4
Danil <vda@2i.tusur.ru>
2024-09-10 03:01:34 +0700

    add Recaptcha

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { GoogleRecaptchaModule } from '@nestlab/google-recaptcha'
+import { getGoogleRecaptchaConfig } from './config/google-recaptcha.config'
 import { TaskModule } from './task/task.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ReviewModule } from './review/review.module';
-import { ConfigModule } from '@nestjs/config'
 import { CityModule } from './city/city.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TaskModule, AuthModule, UserModule, ReviewModule, CityModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }
+  ), GoogleRecaptchaModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: getGoogleRecaptchaConfig,
+    inject: [ConfigService]
+  }), TaskModule, AuthModule, UserModule, ReviewModule, CityModule],
 })
 export class AppModule { }
