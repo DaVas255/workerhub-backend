97e5f8e67810393f15aa237fe1965021bb6730db
Danil <vda@2i.tusur.ru>
2024-09-10 02:40:19 +0700

    add exceptions for global prefix

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +15,14 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api', {
+    exclude: [
+      { path: 'auth/google', method: RequestMethod.GET },
+      { path: 'auth/redirect', method: RequestMethod.GET },
+      { path: 'verify-email', method: RequestMethod.GET },
+    ]
+  })
+
   app.use(cookieParser())
   app.enableCors({
     origin: ['http://localhost:5173'],
